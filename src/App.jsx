b3fe0ca30756201f319bf5777d7f8ef49042bb41
tega <alessiotegoni4@gmail.com>
2024-11-27 19:09:51 +0100

    [ADD] header, main, footer, Card ui component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Card from "./components/ui/Card/Card";
+import "./index.css";
+function App() {
+  return (
+    <div className="hero">
+      <header>
+        <h1>il mio blog</h1>
+      </header>
+      <main>
+        <Card />
+      </main>
+      <footer>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+        laborum sed, quas atque perspiciatis ex sint similique veritatis dolor
+        voluptatum veniam. Voluptate possimus unde veniam dolorem praesentium,
+        quaerat et autem.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
